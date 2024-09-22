336bb97749c07e87388b256dd3b550f9ce8244c3
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2024-09-22 12:08:43 +0530

    Add files via upload

---
+++ day3task.js
@@ -0,0 +1,17 @@
+var num = 3;
+let person = { name: "bhargavi" };
+console.log(person.name);
+const str = "bhavani";
+console.log(str);
+//block concept
+let greeting = "say Hai ";
+{
+  let greeting = "say Hello ";
+  console.log(greeting);
+  console.log(num);
+}
+console.log(greeting);
+{
+  const y = 7;
+  console.log(y);
+}
